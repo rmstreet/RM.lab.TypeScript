057746db57d0124641bf24a999762c03c829da2e
Rômulo Martins <rm_romulomartins@hotmail.com>
2017-09-09 22:25:05 -0300

    organizing the examples in modules

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,11 @@
-class Spacecraft{
-
-  constructor (public propulsor: string){}
-
-  jumpIntoHyperspace(){
-    console.log(`Entering hyperspace with ${this.propulsor}`)
-  }
-}
+import {Spacecraft, Containership} from './base-ships'
+import {MillenniumFalcon} from './starfighters'
 
 let ship = new Spacecraft('hyperdrive')
 ship.jumpIntoHyperspace()
 
-class MillenniumFalcon extends Spacecraft implements Containership {
-
-  cargoContainers: number
-  constructor(){
-    super('hyperdrive')
-    this.cargoContainers = 4
-  }
-
-  jumpIntoHyperspace(){
-    if (Math.random() >= 0.5){
-      super.jumpIntoHyperspace()
-    }else { console.log('Failed to jump into hyperspace')}
-  }
-}
-
 let falcon = new MillenniumFalcon()
 falcon.jumpIntoHyperspace()
-
-interface Containership {
-
-  cargoContainers: number
-}
 
 let gooForTheJob = ( ship: Containership) => ship.cargoContainers > 2
 console.log(`Is falcon good for the job? ${gooForTheJob (falcon) ? 'yes' : 'no'}`)
