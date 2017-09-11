7736294669558901680cd01588030e0587a77553
Rômulo Martins <rm_romulomartins@hotmail.com>
2017-09-10 23:44:56 -0300

    exemplo: add js library in the project

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import {Spacecraft, Containership} from './base-ships'
 import {MillenniumFalcon} from './starfighters'
 
+import * as _ from "lodash"
+console.log(_.pad("Typescript Examples", 40, "="))
+
 let ship = new Spacecraft('hyperdrive')
 ship.jumpIntoHyperspace()
+
 
 let falcon = new MillenniumFalcon()
 falcon.jumpIntoHyperspace()
